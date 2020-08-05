670cf492136cf79c10eb5f4035513ad636473ba6
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2020-08-05 19:43:29 +0200

    Initial commit: Implement stuff

---
+++ README.md
@@ -0,0 +1,46 @@
+# [lessonsonlife](https://benwiederhake.github.io/lessonsonlife/)
+
+> Incomplete list of stuff everyone should know. Tell me what's missing!
+
+This is supposed to be a collection of advice on [how to live a good life](https://benwiederhake.github.io/lessonsonlife/).
+
+Specifically, all advice should be:
+- Universal
+- Concrete
+- Short
+- Understandable
+- Actionable
+
+What you're currently looking at is the code-repository that builds the website.
+
+## Install
+
+You need python to compile it, and git to push it.
+
+Also, you need `pyyaml`, you can install it like this: `pip install pyyaml`
+
+This repository uses submodules. You should either clone with `--recursive`, or run `git submodule update` to checkout `./gh-pages/`.
+
+## Usage
+
+To build: `./render_html.py`
+
+To upload `git push -C gh-pages`
+
+## License
+
+![This is free and unencumbered software released into the public domain.](LICENSE)
+
+The advice is also meant to be as free and unencumbered as possible.
+
+You don't have to, but I would like to ask you for the following:
+- [Live a good life](https://benwiederhake.github.io/lessonsonlife/)
+- Be kind to the people around you
+- Improve humanity, advance civilization
+- If you somehow manage to make a lot of money from this, consider saying thanks by giving me 1% of that revenue.
+
+## Contribute
+
+Feel free to dive in!
+- [Report a typo/suggestion](https://github.com/BenWiederhake/subint/issues/new)
+- [Propose new advice](https://github.com/BenWiederhake/subint/issues/new)
---
+++ render_html.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+
+import datetime
+import html
+import json
+import shutil
+import uuid
+import yaml
+
+
+LESSONS_FILE = 'lessons.yml'
+COUNT_FIRST = 0
+COUNT_PATTERN = "#{:02}"
+
+TEMPLATE_BASE_FILE = 'template.html'
+TEMPLATE_OVERVIEW = '<li><a class="text-monospace font-weight-light" href="#{uuid}">{number}</a> {title}</li>'
+TEMPLATE_LESSON = '''\
+<h3 id="{uuid}"><a class="text-reset text-monospace font-weight-light" href="#{uuid}">{number}</a> {title}</h3>
+<p><dl class="row">
+{items}
+</dl></p>
+'''
+TEMPLATE_LESSON_ITEM = '''\
+<dt class="col-sm-2">{key}</dt>
+<dd class="col-sm-9">{value}</dd>\
+'''
+TEMPLATE_KEYS = ['Abstract', 'Negative example', 'Positive example', 'Consequence']
+TEMPLATE_IGNORE = set(['Title', 'UUID'])
+
+
+def convert_uuids(lessons):
+    for lesson in lessons:
+        lesson['UUID'] = str(uuid.UUID(lesson['UUID']))
+        assert len(lesson['UUID']) == 2 * 16 + 4, lesson['UUID']
+    for i in range(len(lessons)):
+        for j in range(i):
+            u_i = lessons[i]['UUID']
+            u_j = lessons[j]['UUID']
+            matches = sum(a == b for a, b in zip(u_i, u_j))
+            # Even if you take a thousand UUIDs, and some pessimistic assumptions,
+            # Less than half the time you see 8 matches, and only 5% of the time you see more than that.
+            # If you have about 30 UUIDs, the chance to get more than 4 matches is about 1%.
+            # In general: N^2 (n choose k) * 16^{-k}
+            # All this is about variant 4 UUIDs, of course.
+            if matches > 4 + 4:
+                print('ERROR: These UUIDs seem unrandom. Use actual UUIDs.\nHere are some actually-random ones:')
+                for _ in range(round(len(lessons) * 1.01) + 5):
+                    print(uuid.uuid4().urn)
+                exit(1)
+
+
+def wrap_value(string_or_list):
+    if isinstance(string_or_list, str):
+        return [string_or_list]
+    elif isinstance(string_or_list, list):
+        return string_or_list
+    else:
+        raise ValueError('Value "{}" is neither string nor list. Wat?'.format(string_or_list))
+
+
+def render_html(lessons_dict):
+    overview = []
+    lessons_html = []
+    for (i, lesson) in enumerate(lessons_dict):
+        # Check format
+        for k in TEMPLATE_IGNORE:
+            if k not in lesson.keys():
+                print('ERROR: Missing mandatory key "{}" in lesson {} (entry {}), will crash soon'.format(k, lesson, i))
+        for k in lesson.keys():
+            if k not in TEMPLATE_KEYS and k not in TEMPLATE_IGNORE:
+                print('WARNING: Superfluous key "{}" in lesson {}'.format(k, lesson['UUID']))
+
+        html_uuid = lesson['UUID']
+        html_number = COUNT_PATTERN.format(i + COUNT_FIRST)
+        html_title = html.escape(lesson['Title'])
+
+        html_items = []
+        for key in TEMPLATE_KEYS:
+            if key not in lesson:
+                print('WARNING: Missing recommended key "{}" in lesson {}'.format(key, lesson['UUID']))
+                continue
+            for value in wrap_value(lesson[key]):
+                html_items.append(TEMPLATE_LESSON_ITEM.format(key=key, value=html.escape(value)))
+
+        lessons_html.append(TEMPLATE_LESSON.format(uuid=html_uuid, number=html_number, title=html_title, items='\n'.join(html_items)))
+        overview.append(TEMPLATE_OVERVIEW.format(uuid=html_uuid,number=html_number, title=html_title))
+    with open(TEMPLATE_BASE_FILE) as fp:
+        template_base = fp.read()
+    return template_base.format(
+        overview='\n'.join(overview),
+        lessons='\n'.join(lessons_html),
+        last_build=datetime.datetime.now().astimezone(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M %Z'))
+
+
+def install(filename):
+    shutil.copyfile(filename, 'gh-pages/' + filename)
+
+
+def run():
+    print('In case you need a UUID:', uuid.uuid4().urn)
+    with open(LESSONS_FILE) as fp:
+        lessons_raw = yaml.safe_load(fp)
+    # print(lessons_raw)
+    with open('gh-pages/' + LESSONS_FILE + '.json', 'w') as fp:
+        json.dump(lessons_raw, fp, separators=',:')
+    convert_uuids(lessons_raw)
+    with open('gh-pages/index.html', 'w') as fp:
+        fp.write(render_html(lessons_raw))
+    install('favicon.png')
+    install('favicon.ico')
+    install('bootstrap.min.css')
+    install('LICENSE')
+    install('logo.gif')
+    install(LESSONS_FILE)
+
+
+if __name__ == '__main__':
+    run()
